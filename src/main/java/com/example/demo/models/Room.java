78796efc0208114c5c50a0281d9c78bd227e003d
kiruthika1612 <56331587+kiruthika1612@users.noreply.github.com>
2025-04-22 13:23:05 -0400

    Rooms with model. controllwe and Html

---
+++ src/main/java/com/example/demo/models/Room.java
@@ -0,0 +1,55 @@
+package com.example.demo.models;
+
+public class Room {
+
+	private long id;
+	private String number;
+	private String name;
+	private String info;
+
+	public Room() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Room(long id, String number, String name, String info) {
+		super();
+		this.id = id;
+		this.number = number;
+		this.name = name;
+		this.info = info;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+}
